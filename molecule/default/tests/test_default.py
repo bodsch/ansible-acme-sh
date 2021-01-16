9150cd915b7205ae8606d0ee97f6ac40a1f7feb8
Bodo Schulz <bodo@boone-schulz.de>
2021-01-16 21:40:39 +0100

    fix molecule test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,6 +1,12 @@
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
 import pytest
 import os
 import testinfra.utils.ansible_runner
+
+import pprint
+pp = pprint.PrettyPrinter()
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
@@ -8,15 +14,63 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 @pytest.fixture()
 def get_vars(host):
-    defaults_files = "file=../../defaults/main.yml name=role_defaults"
-    vars_files = "file=../../vars/main.yml name=role_vars"
+    """
 
-    ansible_vars = host.ansible(
-        "include_vars",
-        defaults_files)["ansible_facts"]["role_defaults"]
+    """
+    # cwd = os.getcwd()
+    file_defaults = "file={}/defaults/main.yml name=role_defaults".format("../..")
+    file_vars = "file={}/vars/main.yml name=role_vars".format("../..")
+    file_molecule = "file=./group_vars/all/vars.yml name=test_vars"
 
-    ansible_vars.update(host.ansible(
-        "include_vars",
-        vars_files)["ansible_facts"]["role_vars"])
+    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
 
-    return ansible_vars
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(molecule_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
+
+
+def test_user(host, get_vars):
+
+    user_name = get_vars.get('acme_sh_become_user')
+
+    assert host.group(user_name).exists
+    assert host.user(user_name).exists
+    assert user_name in host.user(user_name).groups
+
+
+@pytest.mark.parametrize("dirs", [
+    "/etc/ssl/ansible",
+])
+def test_directories(host, dirs):
+
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
+
+
+def test_acme_dir(host, get_vars):
+
+    user_name = get_vars.get('acme_sh_become_user')
+
+    dir = "/srv/www/{user}/.well-known/acme-challenge".format(user=user_name)
+
+    d = host.file(dir)
+    assert d.is_directory
+    assert d.exists
+
+
+def test_files(host, get_vars):
+    user_name = get_vars.get('acme_sh_become_user')
+
+    file = "/var/lib/{user}/src/acme.sh/acme.sh".format(user=user_name)
+
+    f = host.file(file)
+    assert f.exists
+    assert f.is_file
